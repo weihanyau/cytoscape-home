f5186610898c037923315dcfb08c26f975034ea3
junxian <junxiansean8@gmail.com>
2024-12-25 11:14:07 +0800

    extracted genedetails

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -22,6 +22,7 @@ import { useModal } from "./context/ModalContext";
 import GeneManiaCard from "./GeneManiaCard";
 import NDExCard from "./NDExCard";
 import { Modal } from "./base/Modal";
+import GeneDetails from "./GeneDetails";
 
 export function Results({ open = false, data, onClose }) {
   const type = data?.type;
@@ -41,35 +42,11 @@ export function Results({ open = false, data, onClose }) {
         </button>
       </div>
       <div className="-mt-2.5 text-center sm:text-left">
-        <DialogTitle
-          as="h2"
-          className="mt-0.5 mb-6 text-xl text-center font-semibold leading-6 text-gray-900"
-        >
+        <DialogTitle as="h2" className="mt-0.5 mb-6 text-xl text-center font-semibold leading-6 text-gray-900">
           {title}
         </DialogTitle>
         {type === "gene" && (
-          <>
-            <div className="mt-2 bg-black text-gray-400 text-left py-2">
-              <p className="flex flex-row items-center px-6">
-                <img
-                  src={organism.image}
-                  alt=""
-                  className="h-8 w-8 brightness-0 invert"
-                />
-                <span className="pl-2 italic">{organism.name}</span>
-                <span className="ml-2">
-                  &#40;{geneNames.length} query gene
-                  {geneNames.length > 1 ? "s" : ""}&#41;
-                </span>
-              </p>
-            </div>
-            <div className="flex flex-col lg:flex-row space-y-2 items-start mt-5 px-6 sm:space-x-4 sm:space-y-0">
-              {geneNames.length > 0 && organism && (
-                <GeneManiaCard genes={geneNames} organism={organism} />
-              )}
-              {geneNames.length > 0 && <NDExCard genes={geneNames} />}
-            </div>
-          </>
+          <GeneDetails geneNames={geneNames} organism={organism} />
         )}
       </div>
     </Modal>
