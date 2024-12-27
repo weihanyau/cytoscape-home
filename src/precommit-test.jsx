192ec97119ca124deda694430bad45f832ca4b65
junxian <junxiansean8@gmail.com>
2024-12-27 14:57:07 +0800

    precommit basic configuration done

--- src/precommit-test.jsx
+++
@@ -1,6 +0,0 @@
-const test1 = () => {
-   const message = 'Hello world'; // Missing semicolon (linting issue)
-   return <h1>{message}</h1>; // Incorrect indentation (formatting issue)
-};
-
-export default test1;
