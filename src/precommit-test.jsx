5ee4e333c85e763f47c001c6d211e44d347e2af0
junxian <junxiansean8@gmail.com>
2024-12-27 14:46:22 +0800

    Test precommit hook

---
+++ src/precommit-test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+   const message = "Hello world"; // Missing semicolon (linting issue)
+return <h1>{message}</h1>;  // Incorrect indentation (formatting issue)
+}
+
+export default App;
