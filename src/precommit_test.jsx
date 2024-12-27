a8d97ac82dd3eea72865144b3ccff5de51caf81f
junxian <junxiansean8@gmail.com>
2024-12-27 15:35:45 +0800

    deleted testing file

--- src/precommit_test.jsx
+++
@@ -1,6 +0,0 @@
-const precommit_test = () => {
-   const message = 'Hello world'; // Missing semicolon (linting issue)
-   return <h1>{message}</h1>; // Incorrect indentation (formatting issue)
-};
-
-export default precommit_test;
