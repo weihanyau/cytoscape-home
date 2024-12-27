d01afef7ea1346c3656834ba394e41d75a978ffa
junxian <junxiansean8@gmail.com>
2024-12-27 14:54:32 +0800

    precommit-test

--- src/precommit-test.jsx
+++ src/precommit-test.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
+const test = () => {
+  const message = 'Hello world'; // Missing semicolon (linting issue)
+  return <h1>{message}</h1>; // Incorrect indentation (formatting issue)
+};
 
-const App = () => {
-   const message = "Hello world" // Missing semicolon (linting issue)
-return <h1>{message}</h1>;  // Incorrect indentation (formatting issue)
-}
-
-export default App;
+export default test;
